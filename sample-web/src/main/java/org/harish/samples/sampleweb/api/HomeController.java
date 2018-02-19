7e47f8cd90c7253fbbc9ff590cafb6954c9a452e
Harish Gurlhosur <harishgurlhosur@gmail.com>
2018-02-19 15:41:14 -0500

    Initial changes

---
+++ sample-web/src/main/java/org/harish/samples/sampleweb/api/HomeController.java
@@ -0,0 +1,13 @@
+package org.harish.samples.sampleweb.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello";
+    }
+}
---
+++ sample-web/src/test/java/org/harish/samples/sampleweb/SampleWebApplicationTest.java
@@ -0,0 +1,10 @@
+package org.harish.samples.sampleweb;
+
+import org.springframework.boot.SpringApplication;
+
+public class SampleWebApplicationTest extends SampleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleWebApplicationTest.class, args);
+    }
+}
